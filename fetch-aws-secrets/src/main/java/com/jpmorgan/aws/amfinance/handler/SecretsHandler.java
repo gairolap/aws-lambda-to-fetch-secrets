cd1fb800364291605b605eaf4e559cb28f190167
Priyank <gairola.priyank@gmail.com>
2021-01-14 19:36:49 +0000

    Updated the code to work code with plain java and no spring dependencies

--- fetch-aws-secrets/src/main/java/com/jpmorgan/aws/amfinance/handler/SecretsHandler.java
+++ fetch-aws-secrets/src/main/java/com/jpmorgan/aws/amfinance/handler/SecretsHandler.java
@@ -5,27 +5,30 @@ package com.jpmorgan.aws.amfinance.handler;
 
 import java.util.Map;
 
-import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
-import org.springframework.stereotype.Component;
-
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.AWSSecretsManagerException;
 import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
 import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
+import com.amazonaws.services.secretsmanager.model.InvalidParameterException;
+import com.amazonaws.services.secretsmanager.model.InvalidRequestException;
+import com.amazonaws.services.secretsmanager.model.ResourceNotFoundException;
 import com.jpmorgan.aws.amfinance.util.ServiceConstants;
 
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 
-@Component("SecretsHandler")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Slf4j
-public class SecretsHandler extends SpringBootRequestHandler<Map<String, String>, Object> {
+public class SecretsHandler implements RequestHandler<Map<String, String>, Object> {
 
-	@Override
+	/**
+	 * Method intercepts and processes the incoming requests.
+	 */
 	public Object handleRequest(Map<String, String> request, Context context) {
 
 		return this.fetchSecretsFromSecertsMngr(request);
@@ -39,32 +42,51 @@ public class SecretsHandler extends SpringBootRequestHandler<Map<String, String>
 	 */
 	private Object fetchSecretsFromSecertsMngr(Map<String, String> request) {
 
-		log.info("Retrieving the secret value for secretId {} from region {}", request.get("secretId"),
-				request.get("region"));
+		try {
+			log.info("Retrieving the secret value for secretId {} from region {}", request.get("secretId"),
+					request.get("region"));
 
-		GetSecretValueResult getSecretValueResult = null;
+			GetSecretValueResult getSecretValueResult = null;
 
-		// Create end-point configuration
-		AwsClientBuilder.EndpointConfiguration endpointConfig = new AwsClientBuilder.EndpointConfiguration(
-				request.get(ServiceConstants.SCRT_MGR_ENDPOINT_KEY), request.get(ServiceConstants.REGION_KEY));
-		// Create client-builder
-		AWSSecretsManagerClientBuilder clientBuilder = AWSSecretsManagerClientBuilder.standard();
-		// Set end-point configuration to client-builder
-		clientBuilder.setEndpointConfiguration(endpointConfig);
-		// Create secrets manager client
-		AWSSecretsManager secretsMgrClient = clientBuilder.build();
+			// Create end-point configuration
+			AwsClientBuilder.EndpointConfiguration endpointConfig = new AwsClientBuilder.EndpointConfiguration(
+					request.get(ServiceConstants.SCRT_MGR_ENDPOINT_KEY), request.get(ServiceConstants.REGION_KEY));
+			// Create client-builder
+			AWSSecretsManagerClientBuilder clientBuilder = AWSSecretsManagerClientBuilder.standard();
+			// Set end-point configuration to client-builder
+			clientBuilder.setEndpointConfiguration(endpointConfig);
+			// Create secrets manager client
+			AWSSecretsManager secretsMgrClient = clientBuilder.build();
 
-		GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
-				.withSecretId(request.get(ServiceConstants.SECRET_ID_KEY));
-		// Fetch the secret value
-		getSecretValueResult = secretsMgrClient.getSecretValue(getSecretValueRequest);
+			GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
+					.withSecretId(request.get(ServiceConstants.SECRET_ID_KEY));
+			// Fetch the secret value
+			getSecretValueResult = secretsMgrClient.getSecretValue(getSecretValueRequest);
 
-		if (getSecretValueResult == null) {
-			return "Error occurred while retrieving the secret from secrets manager!";
+			if (getSecretValueResult == null) {
+				return "Error occurred while retrieving the secret from secrets manager!";
+			}
+
+			return (getSecretValueResult.getSecretString() != null) ? getSecretValueResult.getSecretString()
+					: getSecretValueResult.getSecretBinary();
+		} catch (Exception excp) {
+			if (excp instanceof ResourceNotFoundException) {
+				log.info(ServiceConstants.RES_NT_FOUND, excp);
+				return ServiceConstants.RES_NT_FOUND;
+			} else if (excp instanceof InvalidRequestException) {
+				log.info(ServiceConstants.INVLD_REQUEST, excp);
+				return ServiceConstants.INVLD_REQUEST;
+			} else if (excp instanceof InvalidParameterException) {
+				log.info(ServiceConstants.INVLD_REQUEST_PARAMS, excp);
+				return ServiceConstants.INVLD_REQUEST_PARAMS;
+			} else if (excp instanceof AWSSecretsManagerException) {
+				log.info(ServiceConstants.UNBL_TO_ACCESS_KEY_DTLS, excp);
+				return ServiceConstants.UNBL_TO_ACCESS_KEY_DTLS;
+			} else {
+				log.info(ServiceConstants.TECH_ERR, excp);
+				return ServiceConstants.TECH_ERR;
+			}
 		}
-
-		return (getSecretValueResult.getSecretString() != null) ? getSecretValueResult.getSecretString()
-				: getSecretValueResult.getSecretBinary();
 	}
 
 }
